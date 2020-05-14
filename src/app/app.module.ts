00b019fc5f18189e3dea9871d9edd58bd48ecd6a
PikinAlex <alexis_usiel@hotmail.com>
2020-05-13 18:50:48 -0600

    Principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import {AccordionModule} from 'primeng/accordion';   
+import {ButtonModule} from 'primeng/button'; 
+import {MenuModule} from 'primeng/menu'; 
+
+import { Routes, RouterModule } from '@angular/router';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CabezaComponent } from './cabeza/cabeza.component';
+import { PieComponent } from './pie/pie.component';
+import { IndexComponent } from './index/index.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+const routes: Routes = [
+  {path:'', component: IndexComponent},
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabezaComponent,
+    PieComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenuModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MenuModule,
+    ButtonModule,
+    AccordionModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/index/index.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+  items: MenuItem[]; 
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.items = [
+      {label: 'New', icon: 'pi pi-fw pi-plus'},
+      {label: 'Open', icon: 'pi pi-fw pi-download'},
+      {label: 'Undo', icon: 'pi pi-fw pi-refresh'}
+  ];
+  }
+
+}
